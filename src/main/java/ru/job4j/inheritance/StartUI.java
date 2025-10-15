ac5c6447852ba449afe9f1b2464b41c29a336630
Oklevet <nikstar1235@gmail.com>
2025-10-15 21:26:17 +0300

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/inheritance/StartUI.java
@@ -0,0 +1,8 @@
+package ru.job4j.inheritance;
+
+public class StartUI {
+    public static void main(String[] args) {
+        String name = "Job4j";
+        int age = 6;
+    }
+}
--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,11 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public Item() {
     }
@@ -13,6 +16,9 @@ public class Item {
     public Item(int id, String name) {
     }
 
+    public LocalDateTime getTime() {
+        return this.created;
+    }
 
     public int getId() {
         return id;
