cb4d2b3936fa86fe85766ac98906119b923bdc84
Oklevet <nikstar1235@gmail.com>
2025-10-21 19:46:45 +0300

    Репозиторий под проект Tracker

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,28 @@
+package ru.job4j.oop;
+
+public class Error  {
+     private boolean active;
+     private int status;
+     private String message;
+     public Error (boolean active, int status, String message) {
+     this.active = active;
+     this.status = status;
+     this.message = message;
+     }
+
+     public Error() {
+     }
+
+     public static void main(String[] args) {
+       Error error  = new Error();
+       Error error2 = new Error(true, 1, "Синтаксическая ошибка");
+       error.printInfo();
+       error2.printInfo();
+     }
+
+    public void printInfo() {
+        System.out.println("Активна ли ошибка: " + active);
+        System.out.println("Статус " + status);
+        System.out.println("Сообщение " + message);
+    }
+}
