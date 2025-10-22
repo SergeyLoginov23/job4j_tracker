d80dc8abe9e1740d4a6de1a21b726e3827fc357c
Oklevet <nikstar1235@gmail.com>
2025-10-22 20:16:49 +0300

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -29,6 +29,15 @@ public class Tracker {
         return index != -1 ? items[index] : null;
     }
 
+    public void delete(int id) {
+        int index = indexOf(id);
+        if (index != -1) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
+        }
+    }
+
     public boolean replace(int id, Item item) {
         boolean result = false;
         int oldIndex = indexOf(id);
