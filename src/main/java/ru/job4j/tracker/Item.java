afd8cea80e83d2009a74bce52217b71539d9ee06
Oklevet <nikstar1235@gmail.com>
2025-10-23 22:06:33 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -3,7 +3,6 @@ package ru.job4j.tracker;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-
 public class Item {
     private int id;
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
@@ -45,10 +44,18 @@ public class Item {
 
     @Override
     public String toString() {
-        return "Item{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", created=" + created.format(FORMATTER) +
+        return "Item{"
+                +
+                "id="
+                + id
+                +
+                ", name='"
+                + name
+                + '\''
+                +
+                ", created="
+                + created.format(FORMATTER)
+                +
                 '}';
     }
 }
