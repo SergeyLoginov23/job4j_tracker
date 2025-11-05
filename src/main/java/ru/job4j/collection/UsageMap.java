c478b7e791ccf51ee36a18c553327fa086e0a69f
Oklevet <nikstar1235@gmail.com>
2025-11-05 10:32:26 +0300

    0. Map, HashMap [#212652 #554286]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,8 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("lsa@yandex.ru", "Seregey Loginov");
+        map.put("lsa@yandex.ru", "Seregey Loginov");
+        map.put("lsa@mail.ru", "Seregey Loginov");
         map.put("lsa@mail.ru", "Seregey Loginov");
         for (String key : map.keySet()) {
             String value = map.get(key);
