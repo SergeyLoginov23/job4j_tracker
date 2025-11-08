841aae1ac073f9cf0a3ab6ab9210b31b78f5c59e
Oklevet <nikstar1235@gmail.com>
2025-11-08 18:50:48 +0300

    2.1. Лямбда блок [#249208]

--- src/main/java/ru/job4j/lambda/AttachmentSort.java
+++ src/main/java/ru/job4j/lambda/AttachmentSort.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class AttachmentSort {
     public static void main(String[] args) {
+        String left;
         List<Attachment> attachments = Arrays.asList(
                 new Attachment("image 1", 100),
                 new Attachment("image 2", 34),
