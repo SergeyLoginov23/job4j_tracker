e71eef7f18115766f7a73db403dcb6464a8a5f9e
Oklevet <nikstar1235@gmail.com>
2025-11-10 19:20:58 +0300

    9. Лямбда выражения и Collection Framework [#505026]

--- src/main/java/ru/job4j/hashmap/AnalyzeByMap.java
+++ src/main/java/ru/job4j/hashmap/AnalyzeByMap.java
@@ -3,6 +3,8 @@ package ru.job4j.hashmap;
 import java.util.HashMap;
 
 import java.util.*;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class AnalyzeByMap {
     public static double averageScore(List<Pupil> pupils) {
@@ -32,11 +34,12 @@ public class AnalyzeByMap {
     public static List<Label> averageScoreBySubject(List<Pupil> pupils) {
         List<Label> result = new ArrayList<>();
         HashMap<String, Double> map = new HashMap<>();
+        BiFunction<Double, Double, Double> function = (oldValue, newValue) -> oldValue + newValue;
         int subjNumber = pupils.size();
         for (Pupil pupil : pupils) {
             List<Subject> listSubj = pupil.subjects();
             for (Subject subject : listSubj) {
-                map.put(subject.name(), subject.score() + map.getOrDefault(subject.name(), 0D));
+                map.merge(subject.name(), Double.valueOf(subject.score()), function);
             }
         }
         for (String key : map.keySet()) {
@@ -55,6 +58,7 @@ public class AnalyzeByMap {
             }
             scoreList.add(new Label(pupil.name(), totalSum));
         }
+        System.out.println(scoreList);
         scoreList.sort(Comparator.naturalOrder());
         return scoreList.get(scoreList.size() - 1);
     }
@@ -62,10 +66,11 @@ public class AnalyzeByMap {
     public static Label bestSubject(List<Pupil> pupils) {
         List<Label> scoreList = new ArrayList<>();
         HashMap<String, Double> map = new HashMap<>();
+        BiFunction<Double, Double, Double> function = (oldValue, newValue) -> oldValue + newValue;
         for (Pupil pupil : pupils) {
             List<Subject> listSubj = pupil.subjects();
             for (Subject subject : listSubj) {
-                map.put(subject.name(), subject.score() + map.getOrDefault(subject.name(), 0D));
+                map.merge(subject.name(), Double.valueOf(subject.score()), function);
             }
         }
         for (String key : map.keySet()) {
